69fa6866808cbd4135060c5d6b0ba22aa9eef03e
Ashik70021 <ashik70021@gmail.com>
2024-04-20 00:21:05 +0600

    fix home button

--- src/Component/Navabr.jsx
+++ src/Component/Navabr.jsx
@@ -10,7 +10,7 @@ const Navabr = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Home</a></li>
+                            <li><NavLink to="/"><a>Home</a></NavLink></li>
                             <li><a>Categories</a></li>
                             <li><a>Context</a></li>
                         </ul>
@@ -30,6 +30,6 @@ const Navabr = () => {
             </div>
         </div>
     );
-}; 
+};
 
 export default Navabr;
